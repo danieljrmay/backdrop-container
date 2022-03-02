c8c29194e19025df585effcadc631cbb57bafb7c
Daniel J. R. May <danieljrmay@danieljrmay.com>
2022-03-02 15:42:06 +0000

    Update to match new images

--- backdrop-add-on-devel/destroy-backdrop-add-on-devel-pod.bash
+++ backdrop-add-on-devel/destroy-backdrop-add-on-devel-pod.bash
@@ -1,6 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
+# destroy-backdrop-add-on-devel-pod.bash
+#
+# Author: Daniel J. R. May
+#
+# For more information (or to report issues) go to
+# https://github.com/danieljrmay/backdrop-pod
 
-podman pod stop backdrop-pod
-podman pod rm backdrop-pod
-buildah rm backdrop-fedora
-buildah rmi localhost/backdrop-fedora
+# Environment variables and their default values.
+: "${BACKDROP_POD:=backdrop-add-on-devel-pod}"
+
+podman pod stop $BACKDROP_POD
+podman pod rm $BACKDROP_POD
